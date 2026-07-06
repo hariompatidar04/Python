a92a65782c4d166a83ca334927fec57d61808d7f
Hari om patidar <patidarh930@gmail.com>
2026-07-06 17:50:35 +0000

    first python file hello.py

---
+++ language_basics/hello.py
@@ -0,0 +1,8 @@
+# print("What's your name? ")
+# name=input()
+
+name=input("What's your name?")
+surname=input("what's your second name")
+print("hello,"+name+" "+surname)#hello,hari
+print("hello,",name,surname,sep=" ")#hello, hari
+
